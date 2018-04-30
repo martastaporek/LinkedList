41c38195f00a6417030c5a936ffd7f6cc132158d
martastaporek <staporekmarta1@gmail.com>
2018-04-30 15:20:46 +0200

    Create class, implement add menthod and inner Node

--- src/main/java/SinglyLinkedList.java
+++ src/main/java/SinglyLinkedList.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public class SinglyLinkedList <T> {
 
-public class SinglyLinkedList {
+    private Node node = null;
+    private int length = 0;
+
+    public SinglyLinkedList() {
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void add(T data){
+        if (this.node == null){
+            this.node = new Node(data);
+            length++;
+        }else{
+            this.node.next = new Node(data);
+            length++;
+        }
+
+    }
+
+    private class Node {
+        T data;
+        Node next;
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
